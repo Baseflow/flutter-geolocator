83cfd18514f3481d7c269d414bc9ae7e687bcbb7
Maurits van Beusekom <maurits@baseflow.com>
2020-06-14 17:56:44 +0200

    Added permission enum

---
+++ geolocator_platform_interface/lib/src/enums/location_permission.dart
@@ -0,0 +1,15 @@
+/// Represent the possible location permissions.
+enum LocationPermission {
+  /// Permission to access the device's location is denied by the user.
+  denied,
+  /// Permission to access the device's location is denied for ever. The
+  /// permission dialog will not been shown again until the user updates
+  /// the permission in the App settings.
+  deniedForEver,
+  /// Permission to access the device's location is allowed only while
+  /// the App is in use.
+  whileInUse,
+  /// Permission to access the device's location is allowed even when the 
+  /// App is running in the background.
+  always
+}
