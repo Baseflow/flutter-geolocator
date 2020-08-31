bfe96edc4c8a2f8853a00d0503166fb9c28b18c5
Maurits van Beusekom <maurits@baseflow.com>
2020-08-31 15:39:06 +0200

    Fix formatting issue

--- geolocator_platform_interface/lib/src/enums/location_permission.dart
+++ geolocator_platform_interface/lib/src/enums/location_permission.dart
@@ -1,9 +1,9 @@
 /// Represent the possible location permissions.
 enum LocationPermission {
   /// Permission to access the device's location is denied, but you should
-  /// request permission. 
-  /// 
-  /// On iOS this is the initial status indicting the App has not requested 
+  /// request permission.
+  ///
+  /// On iOS this is the initial status indicting the App has not requested
   /// permission yet.
   /// On Android this is also the initial status, however the user can still
   /// choose to deny permissions for now, meaning the App can still request
@@ -11,7 +11,7 @@ enum LocationPermission {
   denied,
 
   /// Permission to access the device's location is permenantly denied. When
-  /// requestiong permissions the permission dialog will not been shown until 
+  /// requestiong permissions the permission dialog will not been shown until
   /// the user updates the permission in the App settings.
   deniedForever,
 
