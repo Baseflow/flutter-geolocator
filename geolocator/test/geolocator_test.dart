c284f050b0f093f0a3072b3550cae83589d81140
Maurits van Beusekom <maurits@baseflow.com>
2020-09-04 17:19:14 +0200

    Fix static analysis warning

--- geolocator/test/geolocator_test.dart
+++ geolocator/test/geolocator_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:geolocator/geolocator.dart' as geolocator;
