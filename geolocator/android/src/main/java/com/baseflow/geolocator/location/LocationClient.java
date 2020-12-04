41077e2f2ea35cc6c79f143f9cab4e04ef0e43aa
Maurits van Beusekom <maurits@baseflow.com>
2020-12-04 13:25:31 +0100

    Filter false positives

--- geolocator/android/src/main/java/com/baseflow/geolocator/location/LocationClient.java
+++ geolocator/android/src/main/java/com/baseflow/geolocator/location/LocationClient.java
@@ -2,6 +2,8 @@ package com.baseflow.geolocator.location;
 
 import android.app.Activity;
 
+import android.content.Context;
+import android.location.LocationManager;
 import com.baseflow.geolocator.errors.ErrorCallback;
 
 public interface LocationClient {
@@ -18,4 +20,11 @@ public interface LocationClient {
       ErrorCallback errorCallback);
 
   void stopPositionUpdates();
+  
+  default boolean checkLocationService(Context context){
+    LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    boolean gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    boolean network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    return gps_enabled || network_enabled;
+  }
 }
