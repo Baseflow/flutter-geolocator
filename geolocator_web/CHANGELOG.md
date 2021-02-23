488c99c9c3e1b7379b238cae3ac74da641bca5d1
Maurits van Beusekom <maurits@baseflow.com>
2021-02-23 09:03:54 +0100

    Fixed bug requesting permissions on web

--- geolocator_web/CHANGELOG.md
+++ geolocator_web/CHANGELOG.md
@@ -1,10 +1,10 @@
+## 2.0.1
+
+- Solve bug causing error when requesting permissions (see issue [#673](https://github.com/Baseflow/flutter-geolocator/issues/673)). 
+
 ## 2.0.0
 
 - Stable release for null safety.
-
-## 2.0.0-nullsafety.0
-
-- Migrate to null safety.
 
 ## 1.0.0
 
--- geolocator_web/lib/geolocator_web.dart
+++ geolocator_web/lib/geolocator_web.dart
@@ -43,17 +43,19 @@ class GeolocatorPlugin extends GeolocatorPlatform {
 
   @override
   Future<LocationPermission> requestPermission() async {
-    if (_permissions == null) {
+    if (_geolocation == null) {
       throw PlatformException(
         code: 'LOCATION_SERVICES_NOT_SUPPORTED',
         message: 'Location services are not supported on this browser.',
       );
     }
 
-    final html.PermissionStatus result =
-        await _permissions!.request(_permissionQuery);
-
-    return _toLocationPermission(result.state);
+    try {
+      _geolocation!.getCurrentPosition();
+      return LocationPermission.whileInUse;
+    } catch (e) {
+      return LocationPermission.denied;
+    }
   }
 
   @override
