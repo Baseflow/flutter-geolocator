7dbe9167cf64025c2a6465a1184e1df9f9a3bd88
Maurits van Beusekom <maurits@baseflow.com>
2021-04-13 11:40:29 +0200

    Fixed formatting

--- geolocator_platform_interface/lib/src/errors/activity_missing_exception.dart
+++ geolocator_platform_interface/lib/src/errors/activity_missing_exception.dart
@@ -1,8 +1,8 @@
 /// An exception thrown when executing functionality which requires an Android
 /// while no activity is provided.
-/// 
-/// This exception is thrown on Android only and might occur hen running a 
-/// certain function from the background that requires a UI element (e.g. 
+///
+/// This exception is thrown on Android only and might occur hen running a
+/// certain function from the background that requires a UI element (e.g.
 /// requesting permissions or enabling the location services).
 class ActivityMissingException implements Exception {
   /// Constructs the [ActivityMissingException]
@@ -15,8 +15,8 @@ class ActivityMissingException implements Exception {
   String toString() {
     if (message == null || message == '') {
       return 'Activity is missing. This might happen when running a certain '
-             'function from the background that requires a UI element (e.g. '
-             'requesting permissions or enabling the location services).';
+          'function from the background that requires a UI element (e.g. '
+          'requesting permissions or enabling the location services).';
     }
     return message!;
   }
