7b41842daae245c758cbcc6e245f9c7038d8fd3a
Floris Smit <florissmit@Mini-Baseflow.localdomain>
2021-05-28 14:59:03 +0200

    Service status enum added

---
+++ geolocator_platform_interface/lib/src/enums/location_service.dart
@@ -0,0 +1,7 @@
+/// Describes the current state of the location service on the native platform.
+enum ServiceStatus {
+  /// Indicates that the location service on the native platform is enabled.
+  enabled,
+  /// Indicates that the location service on the native platform is enabled.
+  disabled,
+}
