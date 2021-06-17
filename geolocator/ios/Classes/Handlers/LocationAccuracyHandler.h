1fecdaad5f5219232dc665976c2dcdda33715f94
Floris Smit <florissmit@Mac-mini-Baseflow.local>
2021-06-17 14:00:28 +0200

    iOS apprx

---
+++ geolocator/ios/Classes/Handlers/LocationAccuracyHandler.h
@@ -0,0 +1,12 @@
+//
+//  LocationAccuracyHandler.h
+//  Pods
+//
+//  Created by Floris Smit on 11/06/2021.
+//
+
+#ifndef LocationAccuracyHandler_h
+#define LocationAccuracyHandler_h
+
+
+#endif /* LocationAccuracyHandler_h */
---
+++ geolocator/ios/Classes/Handlers/LocationAccuracyHandler.m
@@ -0,0 +1,8 @@
+//
+//  LocationAccuracyHandler.m
+//  geolocator
+//
+//  Created by Floris Smit on 11/06/2021.
+//
+
+#import <Foundation/Foundation.h>
