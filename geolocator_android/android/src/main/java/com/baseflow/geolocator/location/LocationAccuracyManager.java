2489dd3e60aaae0ad0d7ccceafb2cf17525690f6
florissmit <floris1.smit@gmail.com>
2021-10-08 09:52:14 +0200

    Changes to the implementation

---
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/location/LocationAccuracyManager.java
@@ -0,0 +1,27 @@
+package com.baseflow.geolocator.location;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.content.ContextCompat;
+
+import com.baseflow.geolocator.errors.ErrorCallback;
+import com.baseflow.geolocator.errors.ErrorCodes;
+
+public class LocationAccuracyManager {
+
+  public LocationAccuracyStatus getLocationAccuracy(Context context, ErrorCallback errorCallback) {
+    if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED) {
+      return LocationAccuracyStatus.precise;
+    } else if (ContextCompat.checkSelfPermission(
+            context, Manifest.permission.ACCESS_COARSE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED) {
+      return LocationAccuracyStatus.reduced;
+    } else {
+      errorCallback.onError(ErrorCodes.permissionDenied);
+      return null;
+    }
+  }
+}
