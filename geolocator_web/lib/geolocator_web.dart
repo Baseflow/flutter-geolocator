62ae0285969a322e9c8d815dfc30efd94de5fd9a
florissmit1 <floris@baseflow.com>
2021-11-19 15:57:09 +0100

    Formatting

--- geolocator_web/lib/geolocator_web.dart
+++ geolocator_web/lib/geolocator_web.dart
@@ -15,7 +15,6 @@ export 'package:geolocator_platform_interface/geolocator_platform_interface.dart
 ///
 /// This class implements the `package:geolocator` functionality for the web.
 class GeolocatorPlugin extends GeolocatorPlatform {
-
   final GeolocationManager _geolocation;
   final PermissionsManager _permissions;
 
