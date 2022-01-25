878ce5ee39ba974c314395daf5e48f52cd588ff5
Rick Casson <4932864+rickcasson@users.noreply.github.com>
2022-01-25 00:09:50 -0600

    add iOS specific test for getCurrentPosition and fix line formatting

--- geolocator/test/geolocator_test.dart
+++ geolocator/test/geolocator_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:mockito/mockito.dart';
@@ -51,6 +52,13 @@ void main() {
       final position = await Geolocator.getCurrentPosition();
 
       expect(position, mockPosition);
+    });
+
+    test('getCurrentPosition iOS', () async {
+      debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+      final position = await Geolocator.getCurrentPosition();
+      expect(position, mockPosition);
+      debugDefaultTargetPlatformOverride = null;
     });
 
     test('getLocationAccuracy', () async {
