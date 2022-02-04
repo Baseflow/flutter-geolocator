033f92bce77d64df0e132522f1f3af0f4efbf3e2
florissmit1 <464337@student.saxion.nl>
2022-02-04 13:21:24 +0100

    Removed unnessecary import

--- geolocator_platform_interface/test/src/extensions/integer_extensions_test.dart
+++ geolocator_platform_interface/test/src/extensions/integer_extensions_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator_platform_interface/geolocator_platform_interface.dart';
-import 'package:geolocator_platform_interface/src/extensions/extensions.dart';
 
 void main() {
   test('toLocationPermission: Should return denied when 0 is supplied', () {
