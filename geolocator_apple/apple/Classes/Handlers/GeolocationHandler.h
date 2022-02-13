5a81e331f53b9f49e92307d0ff281cbb95e9440c
Hennie Brink <Wackymax@users.noreply.github.com>
2022-02-13 22:06:49 +0200

    Update geolocator_apple/apple/Classes/Handlers/GeolocationHandler.h
    Co-authored-by: Maurits van Beusekom <maurits@baseflow.com>

--- geolocator_apple/apple/Classes/Handlers/GeolocationHandler.h
+++ geolocator_apple/apple/Classes/Handlers/GeolocationHandler.h
@@ -24,8 +24,8 @@ typedef void (^GeolocatorResult)(CLLocation *_Nullable location);
 
 - (void)startListeningWithDesiredAccuracy:(CLLocationAccuracy)desiredAccuracy
                            distanceFilter:(CLLocationDistance)distanceFilter
-                            pauseLocationUpdatesAutomatically:(BOOL)pauseLocationUpdatesAutomatically
-                            showBackgroundLocationIndicator:(BOOL)showBackgroundLocationIndicator
+        pauseLocationUpdatesAutomatically:(BOOL)pauseLocationUpdatesAutomatically
+          showBackgroundLocationIndicator:(BOOL)showBackgroundLocationIndicator
                              activityType:(CLActivityType)activityType
                             resultHandler:(GeolocatorResult _Nonnull)resultHandler
                              errorHandler:(GeolocatorError _Nonnull)errorHandler;
