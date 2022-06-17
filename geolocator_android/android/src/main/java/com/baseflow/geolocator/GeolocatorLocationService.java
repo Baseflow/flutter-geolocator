f5a41e5070041b4899008f5aa013a58d780caafd
Hennie <hennie.e.brink@gmail.com>
2022-06-17 10:43:31 +0200

    Upgrade to latest Android API

--- geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorLocationService.java
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/GeolocatorLocationService.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.net.wifi.WifiManager;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
@@ -131,8 +132,13 @@ public class GeolocatorLocationService extends Service {
   public void disableBackgroundMode() {
     if (isForeground) {
       Log.d(TAG, "Stop service in foreground.");
-      stopForeground(ONGOING_NOTIFICATION_ID);
-      releaseWakeLocks();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            stopForeground(ONGOING_NOTIFICATION_ID);
+        }
+        else {
+            stopForeground(true);
+        }
+        releaseWakeLocks();
       isForeground = false;
       backgroundNotification = null;
     }
