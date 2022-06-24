4a9625593460802fe4262e7370468511b41e4ac3
Wendler <wendler.almeida@ravex.com.br>
2022-06-24 08:26:28 -0300

    fix: Android API 33 deprecation issues

--- geolocator_android/android/src/main/java/com/baseflow/geolocator/permission/PermissionUtils.java
+++ geolocator_android/android/src/main/java/com/baseflow/geolocator/permission/PermissionUtils.java
@@ -18,13 +18,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PermissionUtils {
-
   public static boolean hasPermissionInManifest(Context context, String permission) {
     try {
-      PackageInfo info =
-          context
-              .getPackageManager()
-              .getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS);
+      PackageInfo info = getPackageInfo(context);
       if (info.requestedPermissions != null) {
         for (String p : info.requestedPermissions) {
           if (p.equals(permission)) {
@@ -37,5 +33,18 @@ public class PermissionUtils {
     }
 
     return false;
+  }
+
+  @SuppressWarnings("deprecation")
+  private static PackageInfo getPackageInfo(Context context) throws PackageManager.NameNotFoundException {
+    final PackageManager packageManager = context.getPackageManager();
+    final String packageName = context.getPackageName();
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      return packageManager.getPackageInfo(packageName,
+          PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS));
+    }
+
+    return packageManager.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
   }
 }
