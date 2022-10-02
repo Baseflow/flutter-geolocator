21654c885aed2f99c2b9c74e7e4cce8e9ec7cd2b
suyog2001 <suyog0057@gmail.com>
2022-10-02 10:13:42 +0530

    docs: Fix typo in geolocator_platform_interface/lib/src/enums/location_service.dart

--- geolocator_platform_interface/lib/src/enums/location_service.dart
+++ geolocator_platform_interface/lib/src/enums/location_service.dart
@@ -1,6 +1,6 @@
 /// Describes the current state of the location service on the native platform.
 enum ServiceStatus {
-  /// Indicates that the location service on the native platform is enabled.
+  /// Indicates that the location service on the native platform is disabled.
   disabled,
 
   /// Indicates that the location service on the native platform is enabled.
