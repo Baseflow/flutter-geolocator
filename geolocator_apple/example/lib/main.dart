4e319281036ed4caea3ca7f7119e38bc74cf6f61
Maurits van Beusekom <maurits@vnbskm.nl>
2023-06-05 10:02:08 +0200

    Make sure isLocationServicesEnabled is executed on background chat.

--- geolocator_apple/example/lib/main.dart
+++ geolocator_apple/example/lib/main.dart
@@ -173,6 +173,11 @@ class _GeolocatorWidgetState extends State<GeolocatorWidget> {
                     child: const Icon(Icons.bookmark),
                     onPressed: _getLastKnownPosition,
                   ),
+                  sizedBox,
+                  FloatingActionButton(
+                    child: const Icon(Icons.question_mark),
+                    onPressed: _isLocationServiceEnabled,
+                  ),
                 ],
               ),
             ),
@@ -364,6 +369,16 @@ class _GeolocatorWidgetState extends State<GeolocatorWidget> {
         'No last known position available',
       );
     }
+  }
+
+  Future<void> _isLocationServiceEnabled() async {
+    final bool isLocationServiceEnabled =
+        await geolocatorApple.isLocationServiceEnabled();
+    final String displayValue = isLocationServiceEnabled
+        ? 'Location services are enabled.'
+        : 'Location services are disabled.';
+
+    _updatePositionList(_PositionItemType.log, displayValue);
   }
 
   void _getLocationAccuracy() async {
