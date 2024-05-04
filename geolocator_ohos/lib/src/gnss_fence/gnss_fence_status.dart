bf470511215fcfe4b0af34968a47dd9690bcefdc
zmtzawqlp <zmtzawqlp@live.com>
2024-05-04 09:27:05 +0800

    update pubspec

--- geolocator_ohos/lib/src/gnss_fence/gnss_fence_status.dart
+++ geolocator_ohos/lib/src/gnss_fence/gnss_fence_status.dart
@@ -5,5 +5,5 @@ part of '../geolocator_ohos.dart';
 /// 目前仅支持圆形围栏，并且依赖GNSS芯片的地理围栏功能。
 ///
 /// 应用场景举例：开发者可以使用地理围栏，在企业周围创建一个区域进行广告定位，在不同的地点，在移动设备上进行有针对性的促销优惠。
-///
+/// TODO
 mixin GnssFenceStatusMixin {}
